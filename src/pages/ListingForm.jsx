d14cc218a1e66d314acef22fd1ebadb5c16ee57f
orrans <orrans@gmail.com>
2025-12-25 01:03:57 +0200

    Init first form structure

--- src/pages/ListingForm.jsx
+++ src/pages/ListingForm.jsx
@@ -26,6 +26,65 @@ export function ListingForm() {
                     }}
                 />
             </label>
+
+            <label>Address</label>
+            <div>
+                <input type="text" placeholder="City" />
+                <input type="text" placeholder="Country" />
+                <input type="text" placeholder="Street" />
+            </div>
+
+            <div>
+                <label>Drop file here or click to upload</label>
+            </div>
+
+            <label>
+                Capacity
+                <input type="number" placeholder="0" />
+            </label>
+
+            <label>
+                Bedrooms
+                <input type="number" placeholder="0" />
+            </label>
+
+            <label>
+                Bathrooms
+                <input type="number" placeholder="0" />
+            </label>
+
+            <label>
+                Labels
+                <select>
+                    <option>Select labels</option>
+                </select>
+            </label>
+
+            <label>
+                Property type
+                <select>
+                    <option>Select room type</option>
+                </select>
+            </label>
+
+            <label>
+                Price
+                <input type="number" placeholder="0" />
+            </label>
+
+            <label>
+                Amenities
+                <select>
+                    <option>Select amenities</option>
+                </select>
+            </label>
+
+            <label>
+                Description
+                <textarea placeholder="Description" rows="5" />
+            </label>
+
+            <button>Save</button>
         </div>
     )
 }
