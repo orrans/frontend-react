3efb85e24bdae654e9fc960cb891bae7db4960c0
orrans <orrans@gmail.com>
2025-12-25 11:00:01 +0200

    Fixed explore titles

--- src/cmps/StayExploreList.jsx
+++ src/cmps/StayExploreList.jsx
@@ -1,19 +1,33 @@
 import { Link } from 'react-router-dom'
 import { ChevronRightIcon } from './icons/ChevronRightIcon.jsx'
 import { StayPreview } from './StayPreview.jsx'
+import { getRandomIntInclusive } from '../services/util.service.js'
 
 export function StayExploreList({ stays, title }) {
     const fromDate = new Date()
     const toDate = new Date()
     toDate.setDate(toDate.getDate() + 5)
     const displayedStays = stays.slice(0, 8)
+    const stayType = [
+        'Apartment',
+        'House',
+        'Villa',
+        'Cottage',
+        'Cabin',
+        'Bungalow',
+        'Condo',
+        'Loft',
+        'Townhouse',
+        'Chalet',
+    ]
 
     return (
         <section className="explore-list-container">
             <div className="stay-list-title-row">
                 <h2>
                     <Link>
-                        {title} <ChevronRightIcon strokeWidth={5.33333} />
+                        {stayType[getRandomIntInclusive(0, stayType.length - 1)]} in {displayedStays[0]?.loc.city}
+                        <ChevronRightIcon strokeWidth={5.33333} />
                     </Link>
                 </h2>
             </div>
