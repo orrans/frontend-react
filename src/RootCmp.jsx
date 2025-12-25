46f180d8479e92403b1f1d4e322693cb1108d723
orrans <orrans@gmail.com>
2025-12-25 19:09:31 +0200

    Add lazy loading

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,16 +1,19 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { StayIndex } from './pages/StayIndex.jsx'
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
 import { UserMsg } from './cmps/UserMsg.jsx'
-import { StayDetails } from './pages/StayDetails.jsx'
-import { StayCheckout } from './pages/StayCheckout.jsx'
-import { UserProfile } from './pages/UserProfile'
-import { SearchResults } from './pages/SearchResults.jsx'
-import { Login } from './pages/Login.jsx'
-import { OrderList } from './cmps/OrderList.jsx'
-import { ListingList } from './cmps/ListingList.jsx'
-import { ListingForm } from './pages/ListingForm.jsx'
+import { PlatypusLoader } from './cmps/PlatypusLoader.jsx'
+
+const StayIndex = lazy(() => import('./pages/StayIndex.jsx').then(module => ({ default: module.StayIndex })))
+const StayDetails = lazy(() => import('./pages/StayDetails.jsx').then(module => ({ default: module.StayDetails })))
+const StayCheckout = lazy(() => import('./pages/StayCheckout.jsx').then(module => ({ default: module.StayCheckout })))
+const UserProfile = lazy(() => import('./pages/UserProfile').then(module => ({ default: module.UserProfile })))
+const SearchResults = lazy(() => import('./pages/SearchResults.jsx').then(module => ({ default: module.SearchResults })))
+const Login = lazy(() => import('./pages/Login.jsx').then(module => ({ default: module.Login })))
+const OrderList = lazy(() => import('./cmps/OrderList.jsx').then(module => ({ default: module.OrderList })))
+const ListingList = lazy(() => import('./cmps/ListingList.jsx').then(module => ({ default: module.ListingList })))
+const ListingForm = lazy(() => import('./pages/ListingForm.jsx').then(module => ({ default: module.ListingForm })))
 
 export function RootCmp() {
     return (
@@ -19,18 +22,20 @@ export function RootCmp() {
             <UserMsg />
 
             <main>
-                <Routes>
-                    <Route path="/" element={<StayIndex />} />
-                    <Route path="/stay/:stayId/checkout" element={<StayCheckout />} />
-                    <Route path="/stay/:stayId" element={<StayDetails />} />
-                    <Route path="/stay" element={<SearchResults />} />
-                    <Route path="/user/profile" element={<UserProfile />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/orders" element={<OrderList />} />
-                    <Route path="/listings" element={<ListingList />} />
-                    <Route path="/listings/create" element={<ListingForm />} />
-                    <Route path="/listings/:stayId" element={<ListingForm />} />
-                </Routes>
+                <Suspense fallback={<PlatypusLoader />}>
+                    <Routes>
+                        <Route path="/" element={<StayIndex />} />
+                        <Route path="/stay/:stayId/checkout" element={<StayCheckout />} />
+                        <Route path="/stay/:stayId" element={<StayDetails />} />
+                        <Route path="/stay" element={<SearchResults />} />
+                        <Route path="/user/profile" element={<UserProfile />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/orders" element={<OrderList />} />
+                        <Route path="/listings" element={<ListingList />} />
+                        <Route path="/listings/create" element={<ListingForm />} />
+                        <Route path="/listings/:stayId" element={<ListingForm />} />
+                    </Routes>
+                </Suspense>
             </main>
             <AppFooter />
         </div>
