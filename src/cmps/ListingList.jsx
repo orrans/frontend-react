4313800c6f76a37f546d44e635818abcb7f85afc
orrans <orrans@gmail.com>
2025-12-27 21:46:49 +0200

    Style the listing

--- src/cmps/ListingList.jsx
+++ src/cmps/ListingList.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { ListingPreview } from './ListingPreview'
 import { loadStays } from '../store/actions/stay.actions'
 import { PlatypusLoader } from './PlatypusLoader'
 
-export function ListingList({}) {
+export function ListingList() {
     const [isLoading, setIsLoading] = useState(true)
     const loggedInUser = useSelector((state) => state.userModule.user)
     const listings = useSelector((state) =>
@@ -26,27 +26,33 @@ export function ListingList({}) {
     }, [listings.length])
 
     if (isLoading) return <PlatypusLoader />
+    if (!listings.length) return <div>No listings found</div>
+    
     return (
-        <div className="listing-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Listing</th>
-                        <th>Action</th>
-                        <th>Capacity</th>
-                        <th>Bedrooms</th>
-                        <th>Bathrooms</th>
-                        <th>Price</th>
-                        <th>Location</th>
-                        <th>Date added</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listings.map((listing) => (
-                        <ListingPreview key={listing._id} listing={listing} />
-                    ))}
-                </tbody>
-            </table>
+        <div className="listing-list-container">
+            <h3 className="listing-count">{listings.length} listings</h3>
+            
+            <div className="listings-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Listing</th>
+                            <th>Action</th>
+                            <th>Capacity</th>
+                            <th>Bedrooms</th>
+                            <th>Bathrooms</th>
+                            <th>Price</th>
+                            <th>Location</th>
+                            <th>Date added</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listings.map((listing) => (
+                            <ListingPreview key={listing._id} listing={listing} />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
