135e631ce80ad13d689081ea963cf4090e981b71
orrans <orrans@gmail.com>
2025-12-29 01:11:40 +0200

    Created listing mobile, fixed analytics and ismobile hook

--- src/customHooks/useIsMobile.js
+++ src/customHooks/useIsMobile.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react'
 
 export function useIsMobile({ width = 768 } = {}) {
-    const [currWidth, setCurrWidth] = useState(0)
+    const [currWidth, setCurrWidth] = useState(window.innerWidth)
 
     useEffect(() => {
         function handleResize() {
             setCurrWidth(window.innerWidth)
-            console.log(window.innerWidth)
         }
+        
+        // Set initial width
+        handleResize()
+        
         window.addEventListener('resize', handleResize)
         return () => window.removeEventListener('resize', handleResize)
     }, [])
